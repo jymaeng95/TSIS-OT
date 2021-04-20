06da988a269be41a84e1bf068d1596dff5e7a8cc
jymaeng95 <jymaeng95@gmail.com>
2021-04-20 13:11:49 +0900

    [feat] ADD movie function

--- src/main/java/com/tsis/theater/repository/MovieRepository.java
+++ src/main/java/com/tsis/theater/repository/MovieRepository.java
@@ -7,5 +7,9 @@ import java.util.List;
 
 @Repository
 public interface MovieRepository {
+    //모든 영화 정보 불러오기
     List<Movie> getAllMovies();
+
+    //영화 정보 불러오기
+    Movie getMovie(int movieNo);
 }
--- src/main/java/com/tsis/theater/service/MovieService.java
+++ src/main/java/com/tsis/theater/service/MovieService.java
@@ -5,5 +5,9 @@ import com.tsis.theater.domain.Movie;
 import java.util.List;
 
 public interface MovieService {
+    //모든 영화 정보 가져오기
     List<Movie> getAllMoviesInfo();
+
+    //영화 정보 가져오기
+    Movie getMovieInfo(int movieNo);
 }
