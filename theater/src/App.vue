cd7014936be20c8dde408156f43b744eecd270f6
jymaeng95 <jymaeng95@gmail.com>
2021-04-21 23:56:42 +0900

    [feat] ADD tsis theater main view

---
+++ theater/src/App.vue
@@ -0,0 +1,116 @@
+<template>
+  <div id='app'>
+    <div>
+      
+        <h1 class="welcome-msg">Tsis Theater</h1>
+        <button class="btn btn-primary btn-confirm" @click="showModal = true">예매 확인</button> 
+        <TicketingConfirm v-if="showModal" @close="showModal = false">
+          <!--
+            you can use custom content here to overwrite
+            default content
+          -->
+          <h3 slot="header">custom header</h3>
+        </TicketingConfirm>
+    </div>
+    <div class="row movie-main">
+      <div class="card" style="width: 18rem;">
+        <img src="@/assets/clementain.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{movieInfo[0].title}}</h5>
+          <p class="card-text">상영관 : {{movieInfo[0].room}}</p>
+          <a href="#" class="btn btn-primary btn-reserve">예매 하기</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <img src="@/assets/hero.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{movieInfo[1].title}}</h5>
+          <p class="card-text">상영관 : {{movieInfo[1].room}}</p>
+          <a href="#" class="btn btn-primary btn-reserve">예매 하기</a>
+        </div>
+      </div>
+      <div class="card" style="width: 18rem;">
+        <img src="@/assets/match.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{movieInfo[2].title}}</h5>
+          <p class="card-text">상영관 : {{movieInfo[2].room}}</p>
+          <a href="#" class="btn btn-primary btn-reserve">예매 하기</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+import TicketingConfirm from './components/TicketingConfirm.vue'
+export default {
+  name: 'main',
+  components:{
+    TicketingConfirm 
+      
+    
+  },
+  data () {
+    return {
+      movieInfo :[],
+      showModal : false
+    }
+  },
+  created : function() {
+    const baseURI = 'http://localhost:8080/movie';
+    axios.get(`${baseURI}`)
+    .then((result) => {
+      console.log(result)
+      this.movieInfo = result.data
+    })
+  },
+  methods: {
+    
+  }
+}
+</script>
+<style>
+  * {
+    font-family: 'Cafe24Oneprettynight';
+  
+  }
+  *.btn-primary {
+    background: black;
+    border: black;
+  }
+  body {
+    background:#ece6cc;  
+  }
+  @font-face {
+    font-family: 'Cafe24Oneprettynight';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.1/Cafe24Oneprettynight.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  #app {
+    width : 100%;
+  }
+  .welcome-msg {
+    font-weight: bold;
+    margin-top :10px;
+    text-align: center;
+   
+  }
+  .movie-main {
+    text-align: center;
+    margin-left: 10%;
+    margin-right:10%;
+  }
+  .card {
+    margin:50px;
+    box-shadow: 3px 3px 3px gray;
+  }
+  .btn-confirm {
+    float:right;
+    margin-right: 50px;
+  }
+  .card-img-top{
+    margin-top: 5%;
+  }
+  
+</style>
