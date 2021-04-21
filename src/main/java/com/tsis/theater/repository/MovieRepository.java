bf2e5a0631374c44f6f6a8b052125f495c336dba
jymaeng95 <jymaeng95@gmail.com>
2021-04-21 23:55:09 +0900

    [fix] FIX movie function

--- src/main/java/com/tsis/theater/repository/MovieRepository.java
+++ src/main/java/com/tsis/theater/repository/MovieRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Repository
 public interface MovieRepository {
-    //모든 영화 정보 불러오기
+    //모든 영화 정보 가져오기
     List<Movie> getAllMovies();
 
     //영화 정보 불러오기
@@ -15,4 +15,7 @@ public interface MovieRepository {
 
     //현재 상영중인 영화 개수 가져오기
     int getCurrentScreenCount();
+
+    //상영관 별 영화 정보 가져오기
+    Movie getMovieFromRoom(int room);
 }
